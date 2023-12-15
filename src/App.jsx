6f43faf2a62cdbf72977ac8ae7dda63a9a96be35
yusufaa <yusufaa464@gmail.com>
2023-12-15 14:37:42 +0700

    Separating file & add features
    Membuat file baru untuk tiap fungsi, menambahkan fitur delete per item, delete all, check uncheck, status daftar belanja dan sorting berdasarkan input, nama dan cheklist.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-import { useTransition } from "react";
 import { useState } from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import GroceryList from "./components/GroceryList";
+import Footer from "./components/Footer";
 
 const groceryItems = [{
   id: 1,
@@ -27,90 +30,24 @@ export default function App() {
     setItems([...items, item]);
   }
 
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) => items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item));
+  }
+
+  function handleClearItems() {
+    setItems([]);
+  }
+
   return (
     <div className="app">
       <Header />
       <Form onAddItem={handleAddItem} />
-      <GroceryList items={items} />
-      <Footer />
-
+      <GroceryList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} onClearItems={handleClearItems} />
+      <Footer items={items} />
     </div>
-  );
-}
-
-function Header() {
-  return <h1>My Shopping List</h1>
-}
-function Footer() {
-  return <footer className="stats">Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)</footer>;
-}
-
-function Form({ onAddItem }) {
-
-  const [name, setName] = useState('');
-  const [quantity, setQuantity] = useState('');
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if (!name) return;
-
-    const newItem = { name, quantity, checked: false, id: Date.now() };
-    onAddItem(newItem);
-
-    console.log(newItem);
-    setName('');
-    setQuantity(1);
-
-  }
-
-  const quantityNum = [...Array(2077)].map((_, i) => (
-    <option value={i + 1} key={i + 1}>{i + 1}</option>
-  ));
-
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>Hari ini belanja apa kita?</h3>
-      <div>
-        <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))} >
-          {quantityNum}
-        </select>
-        <input type="text" placeholder="nama barang..." value={name} onChange={(e) => setName(e.target.value)} />
-      </div>
-      <button>Tambah</button>
-    </form>
-  );
-}
-function GroceryList({ items }) {
-  return (
-    <>
-      <div className="list">
-        <ul>
-          {items.map((item) => (
-            <Item item={item} key={item.id} />
-          ))}
-        </ul>
-      </div>
-      <div className="actions">
-        <select>
-          <option value="input">Urutkan berdasarkan urutan input</option>
-          <option value="name">Urutkan berdasarkan nama barang</option>
-          <option value="checked">Urutkan berdasarkan ceklis</option>
-        </select>
-        <button>Bersihkan Daftar</button>
-      </div>
-    </>
-  );
-}
-
-function Item({ item }) {
-  return (
-    <li key={item.id}>
-      <input type="checkbox" checked="true" />
-      <span style={item.checked ? { textDecoration: 'line-through' } : {}}>
-        {item.quantity} {item.name}
-      </span>
-      <button>&times;</button>
-    </li>
   );
 }
